5a8ecab565879a78f87eba0b36e8e8ba6a744bcd
Naufal Rizq Ramadhan <naufalrizq100@gmail.com>
2021-09-26 13:45:44 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld { 
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Naufal Rizq Ramadhan");
+		System.out.println("NIM : 6706213150");
+		System.out.println("Kelas : 45-04");
+		}
+		}
+		
